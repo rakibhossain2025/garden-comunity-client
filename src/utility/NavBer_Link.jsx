7ecb8ff9d30c7eee70b61668b147481198a8f164
rakibhossain2025 <rakibhossain4230@gmail.com>
2025-05-24 23:51:13 +0600

    li remove

--- src/utility/NavBer_Link.jsx
+++ src/utility/NavBer_Link.jsx
@@ -6,13 +6,13 @@ const NavBer_Link = () => {
   const { user } = useContext(UserAuth)
   return (
     <>
-      <li className='list-none'> <NavLink to="/"
-        className={({ isActive }) => isActive ? "text-blue-500 text-xl underline" : "hover:text-green-600 transition duration-300  text-xl "} >Home </NavLink></li>
-      <li className='list-none'> <NavLink to="/explore-gardeners" className={({ isActive }) => isActive ? "text-blue-500 text-xl underline" : "hover:text-green-600 transition duration-300  text-xl "} >Explore Gardeners </NavLink></li >
-      <li className='list-none'> <NavLink to="/browse-tips" className={({ isActive }) => isActive ? "text-blue-500 text-xl underline" : "hover:text-green-600 transition duration-300  text-xl "} >Browse Tips </NavLink></li>
+      <NavLink to="/"
+        className={({ isActive }) => isActive ? "text-blue-500 text-xl underline" : "hover:text-green-600 transition duration-300  text-xl "} >Home </NavLink>
+      <NavLink to="/explore-gardeners" className={({ isActive }) => isActive ? "text-blue-500 text-xl underline" : "hover:text-green-600 transition duration-300  text-xl "} >Explore Gardeners </NavLink>
+      <NavLink to="/browse-tips" className={({ isActive }) => isActive ? "text-blue-500 text-xl underline" : "hover:text-green-600 transition duration-300  text-xl "} >Browse Tips </NavLink>
       {user && <>
-        <li className='list-none'> <NavLink to="/share-a-garden-tip" className={({ isActive }) => isActive ? "text-blue-500 text-xl underline" : "hover:text-green-600 transition duration-300  text-xl "} >Share a Garden Tip </NavLink></li>
-        <li className='list-none'> <NavLink to="/My-tips" className={({ isActive }) => isActive ? "text-blue-500 text-xl underline" : "hover:text-green-600 transition duration-300  text-xl "} >My Tips </NavLink></li>
+        <NavLink to="/share-a-garden-tip" className={({ isActive }) => isActive ? "text-blue-500 text-xl underline" : "hover:text-green-600 transition duration-300  text-xl "} >Share a Garden Tip </NavLink>
+        <NavLink to="/My-tips" className={({ isActive }) => isActive ? "text-blue-500 text-xl underline" : "hover:text-green-600 transition duration-300  text-xl "} >My Tips </NavLink>
       </>}
     </>
   );
